8579a6024ab608da0510724ef19bac850c903cb0
Janithx <janithravisankax@gmail.com>
2022-02-21 15:00:57 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, redirect, jsonify
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, URL
+from flask_gravatar import Gravatar
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
+                    base_url=None)
+db = SQLAlchemy(app=app)
+
+
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(200), nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    seats = db.Column(db.String(200), nullable=False)
+    coffee_price = db.Column(db.String(100), nullable=False)
+
+    def to_dict(self):
+        dictionary = {}
+        for column in self.__table__.columns:
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+
+
+@app.route('/')
+def hello_world():
+    cafes = Cafe.query.all()
+    cafe_list = [cafe.to_dict() for cafe in cafes]
+    return render_template("index.html", cafes=cafe_list)
+
+
+@app.route('/login')
+def login():
+    return render_template('sign in.html')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
